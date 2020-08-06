716052feec808689f59b79cde5c9f15f4be096ea
Evan Pavone <evanrpavone@gmail.com>
2020-08-06 02:54:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,4 +16,21 @@ class Song
     song
   end
 
+  def self.new_by_name(title) #class constructor
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(title) #class constructor
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(title) #class finder
+    result = self.all.detect {|song| song.name == title}
+    result
+  end
+
 end
